5b8f7edf946cf24683c938a2adde3252ca43450c
halid cissé <halidcisse346@gmail.comm>
2023-04-07 00:44:19 +0200

    change icon image alt

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <nav className="navbar">
         <h1 className="logo">HalidGPT</h1>
         <div className="iconWrapper">
-          <img className="darkLightMode" src={lightModeIcon} alt="mode"></img>
+          <img
+            className="darkLightMode"
+            src={lightModeIcon}
+            alt="icon theme"
+          ></img>
         </div>
       </nav>
       <div className="chatInterface">f</div>
