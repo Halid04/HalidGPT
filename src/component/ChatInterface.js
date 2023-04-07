751228562e1ceb8dfa9d2dc14d828a51828ce8b3
halid cissé <halidcisse346@gmail.comm>
2023-04-07 15:49:10 +0200

    new code here

--- src/component/ChatInterface.js
+++ src/component/ChatInterface.js
@@ -1,8 +1,28 @@
+import sendMessageIcon from "./../images/sendMessageIcon.png";
+
 export const ChatInterface = ({ themeIcon }) => {
   return (
     <div
       className="chatInterface"
       style={{ backgroundColor: !themeIcon ? "#08242b" : "#F1F1E6" }}
-    ></div>
+    >
+      <MessageInput themeIcon={themeIcon} />
+    </div>
+  );
+};
+
+const MessageInput = ({ themeIcon }) => {
+  return (
+    <form className="interfaceForm">
+      <input
+        className="messageInput"
+        type="text"
+        placeholder="Ask something..."
+        style={{ backgroundColor: !themeIcon ? "#122C34" : "#FFFFFF" }}
+      />
+      <button className="sendBtn">
+        <img src={sendMessageIcon}></img>
+      </button>
+    </form>
   );
 };
