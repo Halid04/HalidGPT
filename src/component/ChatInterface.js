54c7dd85e5d5944916a74d6856344fc9f1e2c794
halid cissé <halidcisse346@gmail.comm>
2023-04-11 23:02:45 +0200

    updated code here

--- src/component/ChatInterface.js
+++ src/component/ChatInterface.js
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import sendMessageIcon from "./../images/sendMessageIcon.png";
+import { type } from "@testing-library/user-event/dist/type";
 
 export const ChatInterface = ({ themeIcon }) => {
+  const [inputText, setInputText] = useState("");
   return (
     <div
       className="chatInterface"
       style={{ backgroundColor: !themeIcon ? "#08242b" : "#F1F1E6" }}
     >
-      <MessageInput themeIcon={themeIcon} />
-      <ShowingMessages />
+      <MessageInput
+        themeIcon={themeIcon}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
+      <ShowingMessages inputText={inputText} />
     </div>
   );
 };
 
-const MessageInput = ({ themeIcon }) => {
+const MessageInput = ({ themeIcon, inputText, setInputText }) => {
+  const handleInputText = (e) => {
+    setInputText(e.target.value);
+  };
+  const handleSendMsg = (e) => {
+    e.preventDefault();
+    if (inputText == "") {
+      alert("non hai scritto niente!!");
+    } else {
+      console.log(inputText);
+    }
+  };
   return (
     <form className="interfaceForm">
       <textarea
@@ -23,45 +41,19 @@ const MessageInput = ({ themeIcon }) => {
           backgroundColor: !themeIcon ? "#122C34" : "#FFFFFF",
           color: !themeIcon ? "#FFFFFF" : "#000000",
         }}
+        onChange={handleInputText}
       />
-      <button className="sendBtn">
-        <img src={sendMessageIcon}></img>
+      <button className="sendBtn" onClick={handleSendMsg}>
+        <img src={sendMessageIcon} alt="send message"></img>
       </button>
     </form>
   );
 };
 
-const ShowingMessages = () => {
+const ShowingMessages = ({ inputText }) => {
   return (
     <div className="showingMessages">
       <span className="userMessage">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s,
-      </span>
-      <span className="assistanteMessage">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s,
-      </span>
-      <span className="userMessage">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s,
-      </span>
-      <span className="assistanteMessage">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s,
-      </span>
-      <span className="userMessage">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, Lorem Ipsum is simply dummy text of the printing and
-        typesetting industry. Lorem Ipsum has been the industry's standard dummy
-        text ever since the 1500s,
-      </span>
-      <span className="assistanteMessage">
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s,
